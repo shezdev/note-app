228c9cd8eae7654b307e80ce2db800860c4e8812
Shereen Finney <shereenfinney@gmail.com>
2017-02-16 15:02:50 +0000

    Playing with the note-controller and index.html

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 Instructions
 -------------
-
 npm install http-server -g
 http-server
-Open http://localhost:8080/ in your web browser
+Check which address is served e.g. mine says:
+```
+Starting up http-server, serving ./
+Available on:
+  http://127.0.0.1:8080
+  http://192.168.48.80:8080
+```
+So I will open http://127.0.0.1:8080/ in my web browser
+
+In the console
+```
+list = new NoteListModel();
+NoteListModel {NOTELIST: Array[0]}
+list.setNote("This is note 1");
+undefined
+list.setNote("This is note 2");
+undefined
+view = new NoteListView(list);
+NoteListView {}
+list.getHTML();
+VM328:1 Uncaught TypeError: list.getHTML is not a function
+    at <anonymous>:1:6
+view.getHTML();
+"<ul><li><div>This is note 2</div></li><li><div>This is note 2</div></li></ul>"
+```
--- note-controller.js
+++ note-controller.js
@@ -1,14 +1,32 @@
 window.onload = function() {
   console.log(document.getElementById("app"));
 document.getElementById("app").innerHTML = "Howdy There";
-
+// var changetextonpage = document.getElementById("app").innerHTML = "Howdy There";
+// console.log(changetextonpage);
+// return var changetextonpage;
 };
 
 
 
 // window.onload = function() {
 //   var list = NoteListModel();
-//   var view = NoteListView();
-//   notelist.setNote("Favourite drink: seltzer");
-//   document.getElementById("app").innerHTML = view.getHTML(list.getNotes());
+//   list.setNote("Favourite drink: seltzer");
+//   var view = NoteListView(list);
+//   document.getElementById("app").innerHTML = view.getHTML();
 // };
+
+// (function() {
+//
+//  var NOTELIST = [];
+//
+//   NoteListController = function(nlm = new NoteListModel) {
+//         NOTELIST = nlm.setNote("Favourite drink: seltzer");
+//   };
+//
+//   NoteListController.prototype.createView = function(NOTELIST) {
+//     view = new NoteListView(NOTELIST);
+//   };
+//
+//
+//
+// })();
