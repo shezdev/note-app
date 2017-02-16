b60e8ae005d110a3b9e17645572550e04d2c3a49
Shereen Finney <shereenfinney@gmail.com>
2017-02-16 12:01:07 +0000

    Playing with the note-controller and index.html

---
+++ README.md
@@ -0,0 +1,6 @@
+Instructions
+-------------
+
+npm install http-server -g
+http-server
+Open http://localhost:8080/ in your web browser
--- note-controller.js
+++ note-controller.js
@@ -1,6 +1,14 @@
 window.onload = function() {
-  var notelistmodel = NoteListModel();
-  var notelistview = NoteListView();
-  notelist.setNote("Favourite drink: seltzer");
-  document.getElementById("app").innerHTML = notelistview.getHTML(notelist.getNotes());
+  console.log(document.getElementById("app"));
+document.getElementById("app").innerHTML = "Howdy There";
+
 };
+
+
+
+// window.onload = function() {
+//   var list = NoteListModel();
+//   var view = NoteListView();
+//   notelist.setNote("Favourite drink: seltzer");
+//   document.getElementById("app").innerHTML = view.getHTML(list.getNotes());
+// };
